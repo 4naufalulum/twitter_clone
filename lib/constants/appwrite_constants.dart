896eaa5010121f09b30d06791417fca42baade7f
4naufalulum <201240001047@unisnu.ac.id>
2023-06-10 17:49:24 +0700

    Storing Tweets to Appwrite Database & Appwrite Storage

--- lib/constants/appwrite_constants.dart
+++ lib/constants/appwrite_constants.dart
@@ -3,4 +3,8 @@ class AppwriteConstants {
   static const String projectId = '6423b38873015f2d09ad';
   static const String endPoint = 'https://baas.pasarjepara.com/v1';
   static const String usersCollection = '647fd35f75f2fee066e9';
+  static const String tweetsCollection = '648438ece36a7fdc500b';
+  static const String imagesBucket = '64844fbae2f178ef174e';
+  static String imageUrl(String imageId) => 
+  '$endPoint/storage/buckets/$imagesBucket/files/$imageId/view?project=$projectId&mode=admin';
 }
